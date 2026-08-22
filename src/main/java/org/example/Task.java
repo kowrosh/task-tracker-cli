5e5b1c852b5a838154a528b55edb3f1fdf202d03
kourosh <kowroshy2219@gmail.com>
2026-08-22 18:13:16 +0200

    add task model and Status

---
+++ src/main/java/org/example/Task.java
@@ -0,0 +1,58 @@
+package org.example;
+
+import java.time.LocalDateTime;
+
+public class Task {
+
+
+    private  int id;
+    private  String description;
+    private TaskStatus status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Task(int id, String description) {
+        this.id = id;
+        this.description = description;
+        this.status = TaskStatus.TODO;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public int getId() {
+        return id;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+        this.updatedAt = LocalDateTime.now();
+    }
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", description='" + description + '\'' +
+                ", status=" + status +
+                ", createdAt=" + createdAt +
+                ", updatedAt=" + updatedAt +
+                '}';
+    }
+
+
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+        this.updatedAt = LocalDateTime.now();
+    }
+    public TaskStatus getStatus() {
+        return status;
+    }
+}
---
+++ src/main/java/org/example/TaskStatus.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import java.util.Arrays;
+
+public enum TaskStatus {
+     TODO,IN_PROGRESS,DONE;
+
+}
+
