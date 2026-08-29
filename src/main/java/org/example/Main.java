38b474c97675bac557bbc9b26d21e772ad81ff77
kourosh <kowroshy2219@gmail.com>
2026-08-29 18:37:10 +0200

    Add CLI command handling

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,14 +5,65 @@ public class Main {
 
         TaskManager manager = new TaskManager();
 
-        manager.addTask("Java");
-        manager.addTask("Gaming");
-        manager.addTask("Coding");
+        if (args.length > 0) {
+            switch (args[0]) {
+                case "add":
+                    if (args.length > 1) {
+                        manager.addTask(args[1]);
+                    }
+                    break;
+                case "list":
+                    if (args.length == 1) {
+                        for (Task task : manager.getAllTasks()) {
+                            System.out.println(task);
+                        }
+                    } else {
+                        switch (args[1]) {
+                            case "done":
+                                for (Task task : manager.getTasksByStatus(TaskStatus.DONE)) {
+                                    System.out.println(task);
+                                }
+                                break;
 
-        manager.markDone(1);
-        manager.markInProgress(2);
+                            case "todo":
+                                for (Task task : manager.getTasksByStatus(TaskStatus.TODO)) {
+                                    System.out.println(task);
+                                }
+                                break;
 
-        System.out.println(manager.getTasksByStatus(TaskStatus.DONE));
-        System.out.println(manager.getTasksByStatus(TaskStatus.IN_PROGRESS));
-        System.out.println(manager.getTasksByStatus(TaskStatus.TODO));
+                            case "in-progress":
+                                for (Task task : manager.getTasksByStatus(TaskStatus.IN_PROGRESS)) {
+                                    System.out.println(task);
+                                }
+                                break;
+                        }
+                    }
+                    break;
+                case "update":
+                    if (args.length > 2) {
+                        manager.updateTask(Integer.parseInt(args[1]), args[2]);
+                    }
+                    break;
+                    case "delete":
+                        if (args.length > 1) {
+                        manager.deleteTask(Integer.parseInt(args[1]));
+                        }
+                        break;
+
+                case "mark-done":
+                    if (args.length > 1) {
+                        manager.markDone(Integer.parseInt(args[1]));
+                    }
+                    break;
+                case "mark-in-progress":
+                    if (args.length > 1) {
+                        manager.markInProgress(Integer.parseInt(args[1]));
+                    }
+                    break;
+            }
+
+
+
+        }
+
 }}
